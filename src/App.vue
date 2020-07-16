e314b3c99a995b913e4b1bb76a1a9d460781c427
ItzMeDwii <dwii5359@azalelnation.com>
2020-07-16 15:54:38 +0700

    Add sections.

--- src/App.vue
+++ src/App.vue
@@ -15,9 +15,15 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav mr-auto">
-            <li class="nav-item">
-              <a class="nav-link" href="#"></a>
+            <li class="nav-item mx-1">
+              <a class="nav-link btn btn-dark" v-scroll-to="'#home'" href="#">Home</a>
             </li>
+            <li class="nav-item mx-1">
+              <a class="nav-link btn btn-dark" v-scroll-to="'#tentang'" href="#">Tentang</a>
+            </li>
+            <li class="nav-item mx-1">
+              <a class="nav-link btn btn-dark" v-scroll-to="'#kegiatan'" href="#">Kegiatan & Fasilitas</a>
+            </li>                                   
           </ul>
           <ul class="navbar-nav ml-auto">
             <ThemeChanger/>
@@ -31,9 +37,47 @@
      * Refer to `router/index.ts` for component that will be displayed
      * over a router.
     -->
-    <div class="center">
-      <div class="container" id="main">
-        
+    <div class="center" id="home">
+      <div class="container">
+          <img alt="Clutik logo" style="height: 220px; width: 220px" src="https://cdn.discordapp.com/attachments/521473681566990340/733234555992932403/1594885926650.png">
+          <h1>Selamat Datang di Club TIK</h1>
+          <p>SMA Negeri 2 Kota Mojokerto</p>
+      </div>
+    </div>
+
+    <br/>
+    <div class="container">
+      <hr/>
+    </div>
+    <br/>
+
+    <div class="center"  id="tentang">
+      <div class="container">
+        <h1>Tentang CluTik </h1>
+      </div>
+    </div>
+
+    <br/>
+    <div class="container">
+      <hr/>
+    </div>
+    <br/>
+
+    <div class="center" id="kegiatan">
+      <div class="container">
+        <h1>Kegiatan & Fasilitas</h1>
+      </div>
+    </div>
+
+    <br/>
+    <div class="container">
+      <hr/>
+    </div>
+    <br/>
+
+    <div class="center" id="content3">
+      <div class="container">
+        <h1>Coming Soon</h1>
       </div>
     </div>
   </div>
@@ -47,6 +91,10 @@
 <script lang="ts">
   import Vue from "vue"
   import {Component} from "vue-property-decorator"
+
+  import ScrollTo from "vue-scrollto"
+  Vue.use(ScrollTo);
+  
   /**
    * You can use `@/` as an equivalent for `./src`
    */
